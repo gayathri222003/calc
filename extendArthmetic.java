800e5576c7170be4c299f0f3c1e1a40a293ebeef
Gayathri-039 <01fe22bca039@kletech.ac.in>
2024-03-09 13:19:41 +0530

    Extended call proogram

---
+++ extendArthmetic.java
@@ -0,0 +1,8 @@
+public class extendArthmetic extends cal {
+    void squre(int b){
+        System.out.println("Squre of a number is =" +b*b);
+    }
+    void cube(int b){
+        System.out.println("Cube of a number is =" +b*b*b);
+    }
+}
